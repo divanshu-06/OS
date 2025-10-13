e25ac47e52d58290415d7d9c157252adaa40527e
ParthXplorer <parthchoyal33@gmail.com>
2025-10-13 23:24:42 +0530

    Update dummy_main.h

--- Assignment_3/dummy_main.h
+++ Assignment_3/dummy_main.h
@@ -1,1 +1,17 @@
+#ifndef DUMMY_MAIN_H
+#define DUMMY_MAIN_H
 
+#include <signal.h>
+#include <unistd.h>
+
+int dummy_main(int argc, char **argv);
+
+int main(int argc, char **argv) {
+    raise(SIGSTOP);    
+    int ret = dummy_main(argc, argv);
+    return ret;
+}
+
+#define main dummy_main
+
+#endif 
