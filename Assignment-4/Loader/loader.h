7663d7da1d72c74d3df49f0f1e0864e70845e687
divanshu-06 <jaindivanshu2006@gmail.com>
2025-11-08 23:23:12 +0530

    make files lauch.c and loader.h

--- Assignment-4/Loader/loader.h
+++ Assignment-4/Loader/loader.h
@@ -1,1 +1,22 @@
+/*
+ * No changes are allowed to this file
+ */
 
+#include <stdio.h>
+#include <elf.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include <errno.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+
+
+void load_and_run_elf(char** exe);
+void loader_cleanup();
